2b96c30e5906c3633de012dbdfdd5e35f243804c
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-06-15 19:18:26 +0000

    platina: Update DT2W configuration
    Co-authored-by: Michael Lenskiy <lmichael1003@gmail.com>
    Co-authored-by: Rique Barros <henripaz1998@gmail.com>
    Change-Id: I6a16ca03d796823b15c6ade5276eb22a3810cf64

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wake_gesture"
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
