2c7246ad1f40999268a8add8bb1a5e491bc776be
arter97 <qkrwngud825@gmail.com>
2021-08-31 04:28:09 +0700

    platina: Optimize everything on preopt
    We have plenty of space left at /system
    
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: I33f1374c2266df87e6c789aa4a39f3892651d8f6
    Signed-off-by: D4rkKnight21 <yusuff8621@gmail.com>

--- device.mk
+++ device.mk
@@ -155,6 +155,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
